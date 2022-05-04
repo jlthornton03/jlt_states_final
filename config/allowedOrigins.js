2daa769e32ec0a325ecd8d01823951a8f86c96d4
jlthornton3 <91161036+jlthornton3@users.noreply.github.com>
2022-05-04 13:11:33 -1000

    make it better

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'https://jlt-states-final.herokuapp.com/',
+    'https://jlt-states-final.herokuapp.com/:3500'
 ];
 
 module.exports = allowedOrigins;
